3f98a0c483125b8b021fcdbe2537a07e96f7fe4e
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-07-29 17:51:26 +0700

    refactor: improve selection handling in HoveringToolbar

--- src/components/Editor/HoveringToolbar.tsx
+++ src/components/Editor/HoveringToolbar.tsx
@@ -34,13 +34,16 @@ const HoveringToolbar = () => {
     }
 
     const domSelection = window.getSelection();
-    const domRange = domSelection!.getRangeAt(0);
-    const rect = domRange.getBoundingClientRect();
-    el.style.opacity = "1";
-    el.style.top = `${rect.top + window.pageYOffset - el.offsetHeight}px`;
-    el.style.left = `${
-      rect.left + window.pageXOffset - el.offsetWidth / 2 + rect.width / 2
-    }px`;
+    console.log(domSelection?.rangeCount);
+    if (domSelection && domSelection.rangeCount > 0) {
+      const domRange = domSelection.getRangeAt(0);
+      const rect = domRange.getBoundingClientRect();
+      el.style.opacity = "1";
+      el.style.top = `${rect.top + window.pageYOffset - el.offsetHeight}px`;
+      el.style.left = `${
+        rect.left + window.pageXOffset - el.offsetWidth / 2 + rect.width / 2
+      }px`;
+    }
   });
 
   return (
