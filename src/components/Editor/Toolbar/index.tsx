7efc2f5df4606bc642b9155f7579ac4657c47c15
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-08-01 17:27:04 +0700

    Add "Content" log button in toolbar

--- src/components/Editor/Toolbar/index.tsx
+++ src/components/Editor/Toolbar/index.tsx
@@ -4,12 +4,15 @@ import { TextHOneIcon } from "@phosphor-icons/react/dist/csr/TextHOne";
 import { TextItalicIcon } from "@phosphor-icons/react/dist/csr/TextItalic";
 import { TextStrikethroughIcon } from "@phosphor-icons/react/dist/csr/TextStrikethrough";
 import { TextUnderlineIcon } from "@phosphor-icons/react/dist/csr/TextUnderline";
+import { BugIcon } from "@phosphor-icons/react/dist/csr/Bug";
 import ToggleBlockButton from "./ToggleBlockButton";
 import ToggleMarkButton from "./ToggleMarkButton";
+import { useSlateStatic } from "slate-react";
+import { Button } from "@/components/ui/button";
 
 const Toolbar = () => {
   return (
-    <div>
+    <div className="flex gap-1">
       <ToggleMarkButton format="bold" icon={<TextBIcon weight="bold" />} />
       <ToggleMarkButton
         format="italic"
@@ -26,8 +29,27 @@ const Toolbar = () => {
       <ToggleMarkButton format="code" icon={<CodeIcon weight="fill" />} />
       <ToggleBlockButton format="code" icon={<CodeIcon />} />
       <ToggleBlockButton format="h1" icon={<TextHOneIcon />} />
+      <GetCurrentContentButton />
     </div>
   );
 };
 
 export default Toolbar;
+
+const GetCurrentContentButton = () => {
+  const editor = useSlateStatic();
+
+  const handleMouseDown = () => {
+    console.log(editor.children);
+  };
+
+  return (
+    <Button
+      variant={"destructive"}
+      className="h-8"
+      onMouseDown={handleMouseDown}
+    >
+      <BugIcon /> Content
+    </Button>
+  );
+};
